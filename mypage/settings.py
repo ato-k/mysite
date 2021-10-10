4562f342c641a0f505ec909cb3a80900aa5434cd
Masato Katsura <ktfdvnc4652869@gmail.com>
2021-10-10 11:17:57 +0900

    add14

--- mypage/settings.py
+++ mypage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ua18s#1j0wwbnssspdg55o-826+g%^vi+7oeh51uqf*d!l2&tr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
